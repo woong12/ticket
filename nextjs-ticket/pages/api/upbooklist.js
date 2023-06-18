aaadead5cd2ccce9917ee22ec566b4903f33a787
igiza1213 <igiza1213@gmail.com>
2023-06-19 01:34:09 +0900

    Update upbooklist.js

--- nextjs-ticket/pages/api/upbooklist.js
+++ nextjs-ticket/pages/api/upbooklist.js
@@ -14,21 +14,14 @@ export default async function handler(req, res) {
       const filter = { _id: new ObjectId("648de37c52547832ccd82bf2") };
       const update = { $push: { booked: body } };
       const options = { returnOriginal: false };
-      const check = await collection.find(filter, { _id: 0 });
-      console.log(check.value);
-      if (!check.value.booked.includes(body)) {
-        const result = await collection.findOneAndUpdate(
-          filter,
-          update,
-          options
-        );
 
-        console.log("Modified document:", result.value);
+      const result = await collection.findOneAndUpdate(filter, update, options);
 
-        res.status(200).json({ message: "Book list saved to MongoDB!" });
-      } else {
-        res.status(404).json({ message: "동시 접속 오류" });
-      }
+      console.log("Modified document:", result.value);
+
+      res.status(200).json({ message: "Book list saved to MongoDB!" });
+
+      res.status(404).json({ message: "동시 접속 오류" });
     } catch (error) {
       console.error("Error saving book list to MongoDB:", error);
       res.status(500).json({ message: "Failed to save book list to MongoDB." });
