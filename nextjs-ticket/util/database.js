90eba2742e750e8d8d9dd654fe5c157bada96e2a
igiza1213 <igiza1213@gmail.com>
2023-06-19 00:50:24 +0900

    Update database.js

--- nextjs-ticket/util/database.js
+++ nextjs-ticket/util/database.js
@@ -1,14 +1,14 @@
 import { MongoClient } from "mongodb";
-const url = process.env.MONGODB_URL;
+const uri = process.env.MONGODB_URI;
 const options = { useNewUrlParser: true };
 let connectDB;
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongo) {
-    global._mongo = new MongoClient(url, options).connect();
+    global._mongo = new MongoClient(uri, options).connect();
   }
   connectDB = global._mongo;
 } else {
-  connectDB = new MongoClient(url, options).connect();
+  connectDB = new MongoClient(uri, options).connect();
 }
 export { connectDB };
