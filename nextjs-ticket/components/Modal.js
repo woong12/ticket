b6b0e03ce598321ee9cab1ffcde6a1270117d8da
msg2324 <73478662+igiza1213@users.noreply.github.com>
2023-06-19 01:17:16 +0000

    asdsad

--- nextjs-ticket/components/Modal.js
+++ nextjs-ticket/components/Modal.js
@@ -56,7 +56,7 @@ export default function Modal() {
         <button
           onClick={() => {
             setStudent("next");
-            router.reload();
+            router.refresh();
           }}
         >
           관리자
